5263947a6e090610fa034ed184e105903b19a118
Yiyi Wang <shd101wyy@gmail.com>
2022-06-03 18:49:22 +0800

    feat: Reimplement the home page

--- src/views/commons/NavBar.vue
+++ src/views/commons/NavBar.vue
@@ -11,7 +11,7 @@
             }"
     >
       <div class="container">
-        <a class="navbar-brand" href="https://myobu.tech/"
+        <a class="navbar-brand" href="/"
         ><img
             :src="
                             coloredLogo
@@ -22,6 +22,60 @@
             alt="logo"
             class="img-fluid"
         /></a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            @click="mobileNavClicked"
+            v-bind:class="{ collapsed: collapsed }"
+        >
+          <span class="ti-menu"></span>
+        </button>
+
+        <div class="collapse navbar-collapse main-menu h-auto" v-bind:class="{ show: !collapsed }"
+             id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="/">{{ $t('home') }}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="#gamefi" v-scroll-to="'#gamefi'">GameFi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="#documents" v-scroll-to="'#documents'">{{ $t('Documents') }}</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="#team" v-scroll-to="'#team'">{{ $t('Team') }}</a>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/hikari">Hikari</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/staking">Staking</router-link>
+            </li>
+<!--            <li class="nav-item dropdown">-->
+<!--              <a class="nav-link page-scroll dropdown-toggle" href="#" id="communityDropdown" role="button"-->
+<!--                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--                Community-->
+<!--              </a>-->
+<!--              <div class="dropdown-menu submenu" aria-labelledby="communityDropdown">-->
+<!--                <a class="nav-link" href="https://forum.myobu.io">{{ $t('forum') }}</a>-->
+<!--&lt;!&ndash;                <a class="nav-link page-scroll" href="https://app.myobu.io">{{ $t('dashboard') }}</a>&ndash;&gt;-->
+<!--              </div>-->
+<!--            </li>-->
+<!--            <li class="nav-item dropdown">-->
+<!--              <a class="nav-link page-scroll dropdown-toggle" href="#" id="navbarDropdownHome" role="button"-->
+<!--                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--                <font-awesome-icon :icon="['fas', 'language']" size="lg"/>-->
+<!--              </a>-->
+<!--              <div class="dropdown-menu submenu" aria-labelledby="navbarDropdownHome">-->
+<!--                <a href="#" v-for="entry in languages" :key="entry.title" @click="changeLocale(entry.language)">-->
+<!--                  <flag :iso="entry.flag" v-bind:squared=false /> {{entry.title}}-->
+<!--                </a>-->
+<!--              </div>-->
+<!--            </li>-->
+          </ul>
+
         </div>
       </div>
     </nav>
@@ -31,7 +85,6 @@
 
 <script>
 import i18n from "@/plugins/i18n";
-
 export default {
   props: {
     coloredLogo: {
@@ -42,13 +95,13 @@ export default {
   data: function () {
     return {
       languages: [
-        { flag: "us", language: "en", title: "English" },
-        { flag: "cn", language: "cn", title: "中文" },
-        { flag: "es", language: "es", title: "Español" },
-        { flag: "kr", language: "kr", title: "한국어" },
-        { flag: "de", language: "de", title: "Deutsch" },
-        { flag: "tr", language: "tr", title: "Türkçe" },
-        { flag: "jp", language: "jp", title: "日本語" },
+        {flag: 'us', language: 'en', title: 'English'},
+        {flag: 'cn', language: 'cn', title: '中文'},
+        {flag: 'es', language: 'es', title: 'Español'},
+        {flag: 'kr', language: 'kr', title: '한국어'},
+        {flag: 'de', language: 'de', title: 'Deutsch'},
+        {flag: 'tr', language: 'tr', title: 'Türkçe'},
+        {flag: 'jp', language: 'jp', title: '日本語'}
       ],
       windowTop: 0,
       collapsed: true,
@@ -80,4 +133,3 @@ export default {
   },
 };
 </script>
-
